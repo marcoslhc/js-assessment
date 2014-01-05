cc363915da0205f36d444ec2517584276f17d811
marcoslhc <marcoslhc@gmail.com>
2014-01-05 17:37:57 -0400

    Array 3 to 11 ready

--- app/arrays.js
+++ app/arrays.js
@@ -23,39 +23,60 @@ define(function() {
     },
 
     remove : function(arr, item) {
-        
+        var newArray = [];
+        each(arr,function(elem){
+            if(elem!=item) newArray.push(elem)
+        });
+        return newArray;
     },
 
     removeWithoutCopy : function(arr, item) {
-
+        var i = 0;
+        do{
+            if(arr[i]==item){
+                arr.splice(i,1)
+            }else{
+                i++
+            }
+        }while(i < arr.length);
+        return arr
     },
 
     append : function(arr, item) {
-
+        arr.push(item);
+        return arr
     },
 
     truncate : function(arr) {
-
+        arr.pop();
+        return arr
     },
 
     prepend : function(arr, item) {
-
+        arr.unshift(item);
+        return arr
     },
 
     curtail : function(arr) {
-
+        arr.shift();
+        return arr
     },
 
     concat : function(arr1, arr2) {
-
+        return arr1.concat(arr2)
     },
 
     insert : function(arr, item, index) {
-
+        arr.splice(index,0,item);
+        return arr
     },
 
     count : function(arr, item) {
-
+        var c=0;
+        each(arr,function(elem){
+            if(elem==item) c++
+        });
+        return c
     },
 
     duplicates : function(arr) {
